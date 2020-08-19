c044aa59a0f1c151765e7d053e5c3b049d23d1f1
Ugenteraan <m.ugenteraan15@gmail.com>
2020-08-19 18:48:00 +0800

    change parameter logic

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ class Model:
     VGG-16 transfer learning model.
     '''
 
-    def __init__(self, model_download_path, new_model_name, input_feature_size, num_class, pretrained_download=True, freeze_feature_layer=False):
+    def __init__(self, model_download_path, new_model_name, input_feature_size, num_class, pretrained_download=True, freeze_feature_layer=True):
         '''
         Initialize VGG-16 from TorchVision
         '''
@@ -24,7 +24,7 @@ class Model:
         self.vgg_model.load_state_dict(model_params) #load the parameters into the architecture.
 
         for feat_layer in self.vgg_model.features.parameters(): #control the gradient flow in the feature layers.
-            feat_layer.requires_grad = freeze_feature_layer
+            feat_layer.requires_grad = not freeze_feature_layer
 
         print("Model feature parameters have been loaded from the pre-trained weight file.")
 
